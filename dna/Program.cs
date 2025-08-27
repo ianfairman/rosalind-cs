04c73c4091719637f1fed8fa8ea29df52494d58d
Ian <ian.fairman@gmail.com>
2025-08-27 13:06:27 +0100

    Solution for rna

---
+++ dna/Program.cs
@@ -0,0 +1,13 @@
+﻿string? dnaString = Console.ReadLine();
+int a = 0; int c = 0; int g = 0; int t = 0;
+foreach (char? nucleotide in dnaString)
+{
+    switch (nucleotide)
+    {
+        case 'A': a++; break;
+        case 'C': c++; break;
+        case 'G': g++; break;
+        case 'T': t++; break;
+    }
+}
+Console.WriteLine($"{a} {c} {g} {t}");
