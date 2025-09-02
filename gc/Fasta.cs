0aad7da3729d604719e578e07d36cd3722c40195
Ian <ian.fairman@gmail.com>
2025-09-02 11:23:21 +0100

    Extract Fasta code.

---
+++ gc/Fasta.cs
@@ -0,0 +1,127 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Fasta
+{
+    public record FastaSequence(string Value)
+    {
+        private int CountGc() => CountChar('C') + CountChar('G');
+
+        public double PercentGc() => (double)CountGc() / (double)Value.Length * 100d;
+
+        private int CountChar(char c)
+        {
+            int count = 0;
+            foreach (char ch in Value)
+            {
+                if (ch == c)
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+
+        public override string ToString() => Value;
+    }
+
+    public record FastaId(string Value)
+    {
+        public override string ToString() => Value;
+    }
+
+    public record FastaRecord(FastaId Id, FastaSequence Sequence)
+    {
+        public override string ToString() => $@"{Id}
+    {PercentGc():F6}";
+
+        public double PercentGc() => Sequence.PercentGc();
+    }
+
+    public record FastaFile(Dictionary<FastaId, FastaSequence> Records)
+    {
+        public FastaRecord MaxGc()
+        {
+            var entryWithMaxValue = Records.MaxBy(entry => entry.Value.PercentGc());
+            return new FastaRecord(entryWithMaxValue.Key, entryWithMaxValue.Value);
+        }
+    }
+
+    public class FastaParser
+    {
+        public FastaFile Parse(TextReader textReader)
+        {
+            string? line = textReader.ReadLine();
+            while (line != null)
+            {
+                ProcessLine(line);
+                line = textReader.ReadLine();
+            }
+            EndOfFile();
+            return new FastaFile(lookup);
+        }
+
+        private void ProcessLine(string line)
+        {
+            if (line.StartsWith('>'))
+            {
+                ProcessId(line);
+                return;
+            }
+            if (line.Trim().Length == 0)
+            {
+                ProcessBlank(line);
+                return;
+            }
+            ProcessSequence(line);
+        }
+
+        private string? id = null;
+        private string sequence = "";
+
+        private void ProcessId(string line)
+        {
+            if (id != null)
+            {
+                HandleRecord(id, sequence);
+            }
+            char[] prefix = { '>' };
+            id = line.TrimStart(prefix).Trim();
+            sequence = "";
+        }
+
+        private void ProcessBlank(string line)
+        {
+            if (id != null)
+            {
+                HandleRecord(id, sequence);
+                id = null;
+                sequence = "";
+            }
+        }
+
+        private void ProcessSequence(string line)
+        {
+            sequence += line.Trim();
+        }
+
+        private void EndOfFile()
+        {
+            if (id != null)
+            {
+                HandleRecord(id, sequence);
+            }
+        }
+
+        private Dictionary<FastaId, FastaSequence> lookup = new();
+
+        private void HandleRecord(string id, string sequence)
+        {
+            FastaId fastaId = new(id);
+            FastaSequence fastaSequence = new(sequence);
+            lookup.Add(fastaId, fastaSequence);
+        }
+    }
+}
